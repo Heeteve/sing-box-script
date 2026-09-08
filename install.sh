c8a3bdca1db9719a9a34dbdcbe983e4625169e42
Heeteve <a13267110880@qq.com>
2026-09-08 19:47:30 +0800

    feat: add hy2

--- install.sh
+++ install.sh
@@ -452,6 +452,7 @@ main() {
     load core.sh
     # 创建默认 Reality 和 Hysteria2 节点，并固定监听端口
     add reality 57777
+    add hy2 57778 auto
     # wait for background tasks (e.g., OpenRC service start)
     wait
     # remove tmp dir and exit.
